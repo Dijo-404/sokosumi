183eea7e3896be93fea0f94a7293b92af17ba5a6
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2025-02-26 12:38:49 +0100

    feat(web-app): added advanced auth features via plugins

--- web-app/src/lib/auth.client.ts
+++ web-app/src/lib/auth.client.ts
@@ -1,4 +1,12 @@
+import { adminClient, multiSessionClient, organizationClient, passkeyClient, twoFactorClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 export const authClient = createAuthClient({
     baseURL: '/',
+    plugins: [
+        organizationClient(),
+        adminClient(),
+        twoFactorClient(),
+        passkeyClient(),
+        multiSessionClient(),
+    ]
 });
