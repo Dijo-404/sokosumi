5c8fcf198e4c999e878918bc51f88f64d336ce84
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2025-03-25 21:40:35 +0900

    feat: Split into envConfig and envSecret and enforced global use via eslint (#133)

--- web-app/src/lib/better-auth/data.ts
+++ web-app/src/lib/better-auth/data.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
-import { PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH } from "@/constants";
+import { envConfig } from "@/config/env.config";
 
 export const nameSchema = (t?: IntlTranslation<"Auth.Schema">) =>
   z
@@ -21,8 +21,8 @@ export const passwordSchema = (t?: IntlTranslation<"Auth.Schema">) =>
   z
     .string({ message: t?.("Password.invalid") })
     .nonempty({ message: t?.("Password.required") })
-    .min(PASSWORD_MIN_LENGTH, { message: t?.("Password.min") })
-    .max(PASSWORD_MAX_LENGTH, { message: t?.("Password.max") })
+    .min(envConfig.PASSWORD_MIN_LENGTH, { message: t?.("Password.min") })
+    .max(envConfig.PASSWORD_MAX_LENGTH, { message: t?.("Password.max") })
     .refine((value) => /^(?=.*[a-z])/.test(value), {
       message: t?.("Password.lowercase"),
     })
