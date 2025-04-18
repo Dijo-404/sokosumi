8d10c7ca9dd01eae121e78e1c7a2f0f86d64cdd4
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2025-04-18 08:34:24 +0200

    refactor(landing): simplify header and navigation, remove mobile sheet navigation (#414)

--- web-app/src/app/(landing)/components/navigation.tsx
+++ web-app/src/app/(landing)/components/navigation.tsx
@@ -2,8 +2,8 @@ import NavigationMenu from "./navigation-menu";
 
 export default function Navigation() {
   return (
-    <nav className="mx-4 hidden flex-1 lg:block">
-      <NavigationMenu className="flex-row items-center justify-center gap-x-6" />
+    <nav className="mx-4 block flex-1">
+      <NavigationMenu className="flex-row items-center justify-center gap-x-6 text-xs md:text-base" />
     </nav>
   );
 }
